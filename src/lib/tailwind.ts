01593061f31a590c44aba3e076cc077a37effdab
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2026-04-24 11:45:31 +0700

    feat: add Tailwind CSS utility functions and typography configuration

---
+++ src/lib/tailwind.ts
@@ -0,0 +1,35 @@
+import clsx, { type ClassValue } from 'clsx';
+import { extendTailwindMerge } from 'tailwind-merge';
+import { createTV } from 'tailwind-variants';
+
+const typographyConfig = {
+  title: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+  label: ['xl', 'lg', 'md', 'sm', 'xs'],
+  paragraph: ['xl', 'lg', 'md', 'sm', 'xs'],
+  subheading: ['md', 'sm', 'xs', '2xs'],
+  doc: ['label', 'paragraph'],
+};
+
+const typographyPatterns = Object.entries(typographyConfig).flatMap(([category, sizes]) =>
+  sizes.map((size) => `${category}-${size}`),
+);
+
+export const twMergeConfig = {
+  extend: {
+    classGroups: {
+      'font-size': [
+        {
+          text: typographyPatterns,
+        },
+      ],
+    },
+  },
+};
+
+const customTwMerge = extendTailwindMerge(twMergeConfig);
+
+export function cn(...classes: ClassValue[]) {
+  return customTwMerge(clsx(...classes));
+}
+
+export const tv = createTV({ twMergeConfig });
