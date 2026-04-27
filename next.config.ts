8ac92475017d81dc2cad90e7247ccbdbdc00b863
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2026-04-27 11:32:20 +0700

    feat: enhance Next.js configuration with MDX support and page redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
-import type { NextConfig } from "next";
+import createMDX from '@next/mdx';
+import type { NextConfig } from 'next';
+import { version } from './package.json';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/docs',
+        permanent: true,
+      },
+    ];
+  },
+  env: {
+    version,
+  },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: ['remark-gfm'],
+    rehypePlugins: ['rehype-slug', 'rehype-mdx-code-props', ['rehype-autolink-headings', { behavior: 'prepend' }]],
+  },
+});
+
+export default withMDX(nextConfig);
