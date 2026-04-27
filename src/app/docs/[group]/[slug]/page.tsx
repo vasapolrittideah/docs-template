d9907f5a8001cc838505d72e3cbe177a6f284409
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2026-04-27 11:34:07 +0700

    feat: implement DocPage and enhance layout with sidebar and font support

---
+++ src/app/docs/[group]/[slug]/page.tsx
@@ -0,0 +1,45 @@
+import { TOCInitializer } from '@/components/docs/toc-initializer';
+import { getDocPage, getGroupMeta, getTOCHeadings, listDocPages } from '@/lib/mdx';
+
+interface DocPageProps {
+  params: Promise<{ group: string; slug: string }>;
+}
+
+const DocPage = async ({ params }: DocPageProps) => {
+  const { group, slug } = await params;
+  const [{ component: MDXComponent, metadata, rawContent, lastModified, lastAuthor }, groupMeta] = await Promise.all([
+    getDocPage(group, slug),
+    getGroupMeta(group),
+  ]);
+  const headings = getTOCHeadings(rawContent);
+
+  return (
+    <>
+      <TOCInitializer headings={headings} />
+      <MDXComponent />
+    </>
+  );
+};
+
+export default DocPage;
+
+export const generateStaticParams = async () => {
+  const docPages = await listDocPages();
+
+  return docPages.map((page) => {
+    return {
+      group: page.group,
+      slug: page.slug,
+    };
+  });
+};
+
+export const generateMetadata = async ({ params }: DocPageProps) => {
+  const { group, slug } = await params;
+  const { metadata } = await getDocPage(group, slug);
+
+  return {
+    title: metadata.title,
+    description: metadata.description,
+  };
+};
